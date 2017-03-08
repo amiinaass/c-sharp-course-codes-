14f44a27693961363cf5485eec93af34f02f61e3
amiinaass <amiinaass7@gmail.com>
2017-03-08 17:09:38 +0000

    first c sharp programme

---
+++ first programme/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_programme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is my first console programme 
+            Console.WriteLine("hello world");
+            Console.ReadKey();       
+                }
+    }
+}
---
+++ first value/first valu/first valu/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_valu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine("my first value = (0)", x);
+            Console.ReadKey();
+        }
+    }
+}
