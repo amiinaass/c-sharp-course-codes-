72e316c871abc0dcbc22356ac19d03883f9197e7
amiinaass <amiinaass7@gmail.com>
2017-03-30 16:42:03 +0100

    this is a sequel for lesson of variable
    text (char , string ) boolean

---
+++ datatypes text/datatypes text/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datatypes_text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charmin = char.MinValue;
+            int charmax = char.MaxValue;
+
+            char myvar = 'Z';
+            int myvarvalue = 'Z';
+            char myvarvalue2 = Convert.ToChar(90);
+            Console.WriteLine("minchar = {0} maxchar = {1} my character = {2} my variable value = {3}  my conversion is = {4} "
+                , charmin, charmax, myvar,myvarvalue, myvarvalue2);
+
+            string myfirststring = ("this is my string variable");
+            Console.WriteLine("Amine Assaddiki {0}", myfirststring);
+
+            bool mybool = true;
+            Console.WriteLine("my boolean value is {0}", mybool);
+            Console.ReadKey();
+        }
+    }
+}
