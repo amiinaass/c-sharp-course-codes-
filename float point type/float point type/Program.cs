999ff6c0b115ef6637d73579f12d9058f0a7f2ed
amiinaass <amiinaass7@gmail.com>
2017-03-30 01:34:05 +0100

    datatypes float point
    this a program to explain a float type

---
+++ float point type/float point type/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace float_point_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal dcmin = decimal.MinValue;
+            decimal dcmax = decimal.MaxValue;
+
+            Console.WriteLine(" min value = {0} max value = {1}", dcmin, dcmax);
+            //hamdolillah
+            //                 sobhane allah
+            Console.ReadKey();
+        }
+    }
+}
