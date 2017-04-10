43732bf93053d517faae672831cec08fccb6a134
amiinaass <amiinaass7@gmail.com>
2017-04-10 15:55:06 +0100

    expression
    first lesson for learning about mathematical operation

---
+++ operator operund expression/operator operund expression/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operator_operund_expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myvar = 10.5f; //name of this sentence is operand
+            float myvar2 = 10.7f; // it's operund 2 
+
+            float temp = myvar + myvar2; // this is expression and (+) is operator 
+
+            //basic operator (+ - * / ) 
+            // bitwise operator 
+            // logical operator boolean ( true , false ) 
+
+            // 1 - unary operand 
+            // 2 - binary operands 
+            // 3 - ternary operands
+
+            Console.WriteLine("{0} {1} {2}" ,myvar ,myvar2, temp);
+            Console.ReadKey();
+
+        }
+    }
+}
