b0a4843e5993478758e1ca31217624a43a3b2c9d
amiinaass <amiinaass7@gmail.com>
2017-04-18 01:12:49 +0100

    increment and decrement
    increment and decrement operatione + assignement operation

---
+++ increment - decrement operations/increment - decrement operations/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace increment___decrement_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 7, myVar2 = 2;
+            myVar1  /= myVar2; 
+            //myVar1 *= myVar2; this operation means myVar1 = myVar1 * myVar2 
+            //myVar1 /= myVar2; this operation means myVar1 = myVar1 / myVar2 
+            // the same concept for plus and minus 
+            Console.WriteLine("{0}", myVar1);
+            Console.ReadKey();
+        }
+    }
+}
