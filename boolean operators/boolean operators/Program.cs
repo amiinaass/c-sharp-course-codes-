daf7cf2e16e9749288f3ff52fa354d55a61a72b4
amiinaass <amiinaass7@gmail.com>
2017-04-18 01:55:41 +0100

    boolean
    boolean comparison and conditions

---
+++ boolean operators/boolean operators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boolean_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boolean comparison == , != , < , > , <= , >= 
+            // Boolean Condition && , || 
+
+            bool myResult;
+            int Var1 = 9, Var2 = 11;
+            // myResult = Var1 == 9 ; // this operations is for indicate if the operation is true or false
+            // myResult = Var1 != 9;
+            //myResult = Var1 <= 8; 
+
+            myResult = (Var1 == 9) || (Var2 == 16);
+            Console.WriteLine("{0}", myResult);
+            Console.ReadKey();
+        }
+    }
+}
